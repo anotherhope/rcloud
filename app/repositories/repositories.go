d33ff258f9190a530546de6de7e56d013152e32c
Florent <florent.hagard@gmail.com>
2022-02-05 13:23:15 +0100

    Update

--- app/repositories/repositories.go
+++ app/repositories/repositories.go
@@ -2,22 +2,55 @@ package repositories
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/anotherhope/rcloud/app/config"
 )
 
-func Contains(name string) bool {
-	for _, d := range config.Get().Repositories {
+func parent(name string) bool {
+	repo := config.Get().Repositories
+
+	for _, d := range repo {
+		if strings.HasPrefix(d.Destination, name) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func sub(name string) bool {
+	repo := config.Get().Repositories
+
+	for _, d := range repo {
+		if strings.HasPrefix(name, d.Destination) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func same(name string) bool {
+	repo := config.Get().Repositories
+
+	for _, d := range repo {
 		if name == d.Destination {
 			return true
 		}
 	}
+
 	return false
 }
 
 func Add(d *config.Directory) error {
-	if Contains(d.Destination) {
-		return fmt.Errorf("value already existe %s", d.Name)
+
+	if same(d.Destination) {
+		return fmt.Errorf("destination path already exist as a sync folder ")
+	} else if parent(d.Destination) {
+		return fmt.Errorf("destination path is parent directory of a sync folder ")
+	} else if sub(d.Destination) {
+		return fmt.Errorf("destination path is sub directory of a sync folder ")
 	}
 
 	config.Set("repositories", append(config.Get().Repositories, d))
