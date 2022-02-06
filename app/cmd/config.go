1fed4234ed1434bdc9e1e9a64ee599a07b26f94b
Florent <florent.hagard@gmail.com>
2022-02-07 00:10:16 +0100

    Update

--- app/cmd/config.go
+++ app/cmd/config.go
@@ -9,11 +9,10 @@ import (
 
 func init() {
 	cmd := &cobra.Command{
-		Args:  cobra.ExactValidArgs(0),
 		Use:   "config",
 		Short: "Enter an interactive configuration session.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			sub := exec.Command("rclone", "config")
+			sub := exec.Command("rclone", append([]string{"config"}, args[:]...)...)
 			sub.Stdout = os.Stdout
 			sub.Stdin = os.Stdin
 			sub.Stderr = os.Stderr
