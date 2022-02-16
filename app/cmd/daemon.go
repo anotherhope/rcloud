f06dc636d077af77e842c23c673386f35c69691d
Florent <florent.hagard@gmail.com>
2022-02-16 13:50:13 +0100

    Update

--- app/cmd/daemon.go
+++ app/cmd/daemon.go
@@ -14,7 +14,7 @@ import (
 var daemonCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(0),
 	Use:   "daemon",
-	Short: "Daemon management",
+	Short: "Start Daemon service",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var exit = make(chan os.Signal, 1)
 		signal.Notify(exit, os.Interrupt)
