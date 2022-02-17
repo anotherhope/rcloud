00dd1dd19bb94ebb461d69389e93c9a5320279f6
Florent <florent.hagard@gmail.com>
2022-02-17 14:38:57 +0100

    Update

--- app/cmd/selfupdate.go
+++ app/cmd/selfupdate.go
@@ -16,15 +16,17 @@ var selfUpdate = &cobra.Command{
 	Use:   "selfupdate",
 	Short: "Update Rcloud and Rclone if needed",
 	RunE: func(cmd *cobra.Command, args []string) error {
-
+		fmt.Println("selfupdate")
 		binaryUrl := "https://github.com/anotherhope/rcloud/releases/download/latest/rcloud-" + runtime.GOOS + "-" + runtime.GOARCH
 		hashUrl := binaryUrl + ".md5"
+		fmt.Println("hash:analyse")
 		if hashRemote, err := update.ReadRemote(hashUrl); err == nil {
 			binPath, _ := os.Executable()
 			file, _ := os.Open(binPath)
 			hash := md5.New()
 			io.Copy(hash, file)
 			hashLocal := fmt.Sprintf("%x", hash.Sum(nil))
+			fmt.Println("hash:change")
 			if hashRemote != hashLocal {
 				fmt.Println("Download in progress")
 				update.DownloadFile(
@@ -34,15 +36,19 @@ var selfUpdate = &cobra.Command{
 			}
 		}
 
+		fmt.Println("rclone")
 		sub := exec.Command("rclone", "selfupdate")
 		sub.Stdout = os.Stdout
 		sub.Stdin = os.Stdin
 		sub.Stderr = os.Stderr
+
+		fmt.Println("rclone:start")
 		err := sub.Start()
 		if err != nil {
 			return err
 		}
 
+		fmt.Println("rclone:wait")
 		fmt.Println(sub.Wait())
 
 		return nil
